2309333e17b6ac1a1a9ddec075f0d2d8b3c32ffe
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-02-23 16:56:57 +0530

    Category List & Detail Page Implementation

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,14 +1,24 @@
+const Item = require("../models/item");
 const Category = require("../models/category");
+
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Category.
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category list");
+  
+  const categories = await Category.find().sort({ name: 1 }).exec();
+  res.render("category_list", { title: "Category List", category_list: categories });
 });
 
 // Display detail page for a specific Category.
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
+  const [category, items] = await Promise.all([
+
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }).exec()
+  ]);
+
+  res.render("category_detail", { title: category.name, category: category, items: items });
 });
 
 // Display Category create form on GET.
